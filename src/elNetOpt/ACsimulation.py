8fe4429f8a5f5a2c7c27eb6299d0790cbd858331
mbonvini <mbonvini@lbl.gov>
2014-04-18 09:32:22 -0700

    First versions of optimization with AC running

--- src/elNetOpt/ACsimulation.py
+++ src/elNetOpt/ACsimulation.py
@@ -96,17 +96,33 @@ def run_simulation_with_inputs(time, price, pv, bldg, plot = False):
     # Extract variable profiles
     if plot:
         plotFunction(res)
+        
+    return res
     
-def plotFunction(res):
+def plotFunction(res, res_opt=None):
     
     time = res["time"]/3600.0
     price= res["price"]
+    Vs   = res["S.Vrms"]
     V1   = res["bldg1.Vrms"]
     V2   = res["bldg2.Vrms"]
     V3   = res["bldg3.Vrms"]
     P_batt = res["batt.P"]
     Q_batt = res["batt.Q"]
     SOC    = res["batt.SOC"]
+    E      = res["E"]
+    Money  = res["Money"]
+    
+    if res_opt != None:
+        time_o = res_opt["time"]/3600.0
+        V1_o   = res_opt["bldg1.Vrms"]
+        V2_o   = res_opt["bldg2.Vrms"]
+        V3_o  = res_opt["bldg3.Vrms"]
+        P_batt_o = res_opt["batt.P"]
+        Q_batt_o = res_opt["batt.Q"]
+        SOC_o    = res_opt["batt.SOC"]
+        E_o      = res_opt["E"]
+        Money_o  = res_opt["Money"]
     
     figPrice = plt.figure()
     ax = figPrice.add_subplot(111) 
@@ -119,31 +135,131 @@ def plotFunction(res):
     
     figV = plt.figure()
     ax = figV.add_subplot(111) 
-    ax.plot(time, V1, 'r', label='$V_1$')
-    ax.plot(time, V2, 'g', label='$V_2$')
-    ax.plot(time, V3, 'b', label='$V_3$')
+    if res_opt == None:
+        ax.plot(time, Vs, 'k', label='$Vs$')
+        ax.fill_between(time, Vs*0.95, Vs*1.05, facecolor='grey', alpha=0.2)
+        ax.plot(time, V1, 'r', label='$V_1$')
+        ax.plot(time, V2, 'g', label='$V_2$')
+        ax.plot(time, V3, 'b', label='$V_3$')
+    else:
+        ax.plot(time, Vs, 'k', label='$Vs$')
+        ax.fill_between(time, Vs*0.95, Vs*1.05, facecolor='grey', alpha=0.2)
+        ax.plot(time, V1, 'r--', label='$V_1$')
+        ax.plot(time, V2, 'g--', label='$V_2$')
+        ax.plot(time, V3, 'b--', label='$V_3$')
+        ax.plot(time_o, V1_o, 'r', label='$V_1^{*}$')
+        ax.plot(time_o, V2_o, 'g', label='$V_2^{*}$')
+        ax.plot(time_o, V3_o, 'b', label='$V_3^{*}$')
     ax.set_xlim([0, 24])
     ax.set_xlabel('time [hours]')
     ax.set_ylabel('Voltage [V]')
-    ax.legend()
+    ax.legend(ncol=4, loc="upper center")
     
     figBatt = plt.figure()
-    ax = figBatt.add_subplot(211) 
-    ax.plot(time, P_batt, 'r', label='$P_{BATT}$')
-    ax.plot(time, Q_batt, 'g', label='$Q_{BATT}$')
+    ax = figBatt.add_subplot(211)
+    if res_opt == None:
+        ax.plot(time, P_batt, 'r', label='$P_{BATT}$')
+        ax.plot(time, Q_batt, 'g', label='$Q_{BATT}$')
+    else:
+        ax.plot(time, P_batt, 'r--', label='$P_{BATT}$')
+        ax.plot(time, Q_batt, 'g--', label='$Q_{BATT}$')
+        ax.plot(time_o, P_batt_o, 'r', label='$P_{BATT}^{*}$')
+        ax.plot(time_o, Q_batt_o, 'g', label='$Q_{BATT}^{*}$')
     ax.set_xlim([0, 24])
     ax.set_xlabel('time [hours]')
     ax.set_ylabel('Power fed into the battery [W]')
-    ax.legend()
+    ax.legend(ncol=2, loc="upper center")
     
-    ax = figBatt.add_subplot(212) 
-    ax.plot(time, SOC, 'r', label='$SOC$')
+    ax = figBatt.add_subplot(212)
+    if res_opt == None:
+        ax.plot(time, SOC, 'k', label='$SOC$')
+    else:
+        ax.plot(time, SOC, 'k--', label='$SOC$')
+        ax.plot(time_o, SOC_o, 'k', label='$SOC^{*}$')
     ax.set_xlim([0, 24])
     ax.set_xlabel('time [hours]')
     ax.set_ylabel('State of Charge [$\cdot$]')
-    ax.legend()
+    ax.legend(ncol=1, loc="upper right")
+    
+    figEM = plt.figure()
+    ax = figEM.add_subplot(211) 
+    if res_opt == None:
+        ax.plot(time, E, 'b', label='$E_{BATT}$')
+    else:
+        ax.plot(time, E, 'b--', label='$E_{BATT}$')
+        ax.plot(time_o, E_o, 'b', label='$E_{BATT}^{*}$')
+    ax.set_xlim([0, 24])
+    ax.set_xlabel('time [hours]')
+    ax.set_ylabel('Energy [kWh]')
+    ax.legend(loc="upper left")
+    
+    ax = figEM.add_subplot(212)
+    if res_opt == None:
+        ax.plot(time, Money, 'b', label='$Money$')
+    else:
+        ax.plot(time, Money, 'b--', label='$Money$')
+        ax.plot(time_o, Money_o, 'b', label='$Money^{*}$')
+    ax.set_xlim([0, 24])
+    ax.set_xlabel('time [hours]')
+    ax.set_ylabel('Money [$]')
+    ax.legend(loc="upper left")
     
     plt.show()
+
+def run_optimization(sim_res, time, price, pv, bldg):
+    """
+    This function runs an optimization problem
+    """
+    
+    from pyjmi.optimization.casadi_collocation import MeasurementData
+    from collections import OrderedDict
+
+    # get current directory
+    curr_dir = os.path.dirname(os.path.abspath(__file__));
+    
+    # compile FMU
+    path = os.path.join(curr_dir,"..","..","Models","ElectricalNetwork.mop")
+    model_name = compile_fmux('ElectricNetwork.ACnetworkBatteryMngmtOpt_Money', path, compiler_options={"enable_variable_scaling":True})
+    
+    # Load the model
+    model_casadi = CasadiModel(model_name) 
+    
+    # Get the inputs that should be eliminated from the optimization variables
+    eliminated = OrderedDict()
+    data_price = np.vstack([t_data, price])
+    eliminated['price'] = data_price
+    
+    data_Q  = np.vstack([t_data, np.zeros(Npoints)])
+    eliminated['Q_batt'] = data_Q
+    
+    data_pv1 = np.vstack([t_data, np.squeeze(pv[:,0])])
+    eliminated['P_pv1'] = data_pv1
+    data_pv2 = np.vstack([t_data, np.squeeze(pv[:,1])])
+    eliminated['P_pv2'] = data_pv2
+    data_pv3 = np.vstack([t_data, np.squeeze(pv[:,2])])
+    eliminated['P_pv3'] = data_pv3
+    
+    data_bldg1 = np.vstack([t_data, -np.squeeze(bldg[:,0])])
+    eliminated['P_bldg1'] = data_bldg1
+    data_bldg2 = np.vstack([t_data, -np.squeeze(bldg[:,1])])
+    eliminated['P_bldg2'] = data_bldg2
+    data_bldg3 = np.vstack([t_data, -np.squeeze(bldg[:,2])])
+    eliminated['P_bldg3'] = data_bldg3
+    
+    measurement_data = MeasurementData(eliminated = eliminated)
+    
+    # define the optimization problem
+    opts = model_casadi.optimize_options()
+    opts['n_e'] = 60
+    opts['measurement_data'] = measurement_data
+    opts['init_traj'] = sim_res.result_data
+    
+    # get the results of the optimization
+    res = model_casadi.optimize(options = opts)
+    
+    plotFunction(sim_res, res)
+    
+    return res
 
 if __name__ == '__main__':
     #run_simulation()
@@ -167,4 +283,7 @@ if __name__ == '__main__':
     (pv, bldg) = getData(t_data, plot = False)
     
     # Run the simulation
-    res = run_simulation_with_inputs(t_data, price, pv, bldg, plot = True)
+    res = run_simulation_with_inputs(t_data, price, pv, bldg, plot = False)
+    
+    # Run the optimization
+    run_optimization(res, t_data, price, pv, bldg)
